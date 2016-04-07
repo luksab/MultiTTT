43e8497019ceb65613b8b2a08de534569b761c81
luksab <luksablp@gmail.com>
2016-04-07 08:20:08 +0200

    remove thread dependency from MyFrameSP

--- MyFrameSP.java
+++ MyFrameSP.java
@@ -2,7 +2,6 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 import java.util.*;
-import java.util.concurrent.*;
 
 public class MyFrameSP extends JFrame implements ActionListener
 {
